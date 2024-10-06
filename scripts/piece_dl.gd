3f27ea9d8b4aa97e70dc712f9fd76dd59ad0d5ea
turncoda <144861042+turncoda@users.noreply.github.com>
2024-10-06 14:39:29 -0700

    Bugfix: deflect out of bounds
    Don't deflect if there's something in the target location

--- scripts/piece_dl.gd
+++ scripts/piece_dl.gd
@@ -4,4 +4,6 @@ extends SinglePiece
 func can_merge(other: SinglePiece) -> bool:
 	return other is PieceUR
 func try_deflect(other: SinglePiece) -> Deflect:
+	if raycast_right.is_colliding():
+		return Deflect.NONE
 	return Deflect.RIGHT
--- scripts/piece_dr.gd
+++ scripts/piece_dr.gd
@@ -4,4 +4,6 @@ extends SinglePiece
 func can_merge(other: SinglePiece) -> bool:
 	return other is PieceUL
 func try_deflect(other: SinglePiece) -> Deflect:
+	if raycast_left.is_colliding():
+		return Deflect.NONE
 	return Deflect.LEFT
