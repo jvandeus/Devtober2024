784da6bdedf7c31dd9c50750a78b27d22ea0748b
turncoda <144861042+turncoda@users.noreply.github.com>
2024-10-27 13:02:39 -0700

    Remove unused parts of Piece

--- scripts/piece.gd
+++ scripts/piece.gd
@@ -3,12 +3,6 @@ extends Node2D
 
 enum Kind { GREEN, RED, BLUE, YELLOW, GARBAGE }
 
-@onready var apparent_transform = $ApparentTransform
-@onready var ray_up = $Up
-@onready var ray_down = $Down
-@onready var ray_left = $Left
-@onready var ray_right = $Right
-@onready var Shape = $Shape
 @onready var change_sfx = $ChangeSFX
 
 @export var kind: Kind
@@ -17,7 +11,6 @@ enum Kind { GREEN, RED, BLUE, YELLOW, GARBAGE }
 const FALL_SPEED := 1000.0 # pixels per second
 const KINDS = [Kind.GREEN, Kind.RED, Kind.BLUE, Kind.YELLOW]
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	idle()
 	
@@ -48,29 +41,6 @@ func fall_to(y: int) -> Tween:
 	tween.tween_property(self, "position", target, distance / FALL_SPEED)
 	return tween
 
-# get immmediately adjacent pieces
-func get_adj_pieces() -> Array:
-	var adj_pieces = []
-	for ray in [ray_down, ray_up, ray_left, ray_right]:
-		var collider = ray.get_collider()
-		if collider is Piece:
-			adj_pieces.append(ray.get_collider())
-	return adj_pieces
-
-# recursive depth-first search to find the contiguous cluster of same-kind pieces touching this one
-func get_cluster(visited := {}) -> Array:
-	visited[hash(self)] = true
-	var result = [self]
-	for piece in get_adj_pieces():
-		if piece.kind == kind and !visited.has(hash(piece)):
-			result.append_array(piece.get_cluster(visited))
-	return result
-
-func update_children() -> void:
-	for ray in [ray_down, ray_up, ray_left, ray_right]:
-		if ray: ray.target_position.x = cell_size
-	if Shape: Shape.scale = Vector2(cell_size / 64.0, cell_size / 64.0)
-
 func set_kind(k: Kind) -> void:
 	change_sfx.play()
 	$AnimatedSprite2D.visible = true
@@ -78,7 +48,3 @@ func set_kind(k: Kind) -> void:
 	await $AnimatedSprite2D.animation_finished
 	$AnimatedSprite2D.visible = false
 	kind = k
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	update_children()
