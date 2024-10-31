db04a035bd894bce608e227d0fdc03cf1d3e6f9c
turncoda <144861042+turncoda@users.noreply.github.com>
2024-10-31 03:02:34 -0700

    Add credits

---
+++ scripts/credits.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$GoBack.grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_go_back_pressed() -> void:
+	var scene = load("res://scenes/title.tscn").instantiate()
+	get_tree().get_root().add_child(scene)
+	queue_free()
--- scripts/title.gd
+++ scripts/title.gd
@@ -24,9 +24,12 @@ func _on_play_button_pressed() -> void:
 	var scene = load("res://scenes/difficulty_select.tscn").instantiate()
 	get_tree().get_root().add_child(scene)
 	queue_free()
-		
-func _on_options_button_pressed() -> void:
-	pass
 	
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_button_pressed() -> void:
+	var scene = load("res://scenes/credits.tscn").instantiate()
+	get_tree().get_root().add_child(scene)
+	queue_free()
